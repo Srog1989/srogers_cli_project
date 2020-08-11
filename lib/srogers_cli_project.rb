4e991f50646f891dc9855fef962014b46194ff8e
Sarah Rogers <s.rogers0715@yahoo.com>
2020-08-11 22:40:50 +0000

    initial setup

---
+++ lib/srogers_cli_project.rb
@@ -0,0 +1,6 @@
+require "srogers_cli_project/version"
+
+module SrogersCliProject
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/srogers_cli_project/version.rb
@@ -0,0 +1,3 @@
+module SrogersCliProject
+  VERSION = "0.1.0"
+end
