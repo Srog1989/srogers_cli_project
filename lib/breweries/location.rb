81ac3b7380cdd465ff62f36abe883c2d1410c062
Sarah Rogers <s.rogers0715@yahoo.com>
2020-08-24 15:56:15 +0000

    updated lib files

--- lib/breweries/location.rb
+++ lib/breweries/location.rb
@@ -1,10 +1,11 @@
 
 class Breweries::Location
-   attr_accessor :name, :brewery_name
+   attr_accessor :name, :brewery_name, :state
    @@all = []
    def initialize(name, brewery_name)
      @name = name
      @brewery_name = brewery_name
+     @state = state
      save
     end 
      
@@ -22,3 +23,7 @@ class Breweries::Location
     # @locations.collect{|name| location.name}.uniq
     # end
 end
+
+#a location is a hash 
+#has keys of state, name, type, etc 
+#those keys would be mirrored in the attributes of your location class, i.e. your attr attr_accessors, etc.
--- lib/breweries/name.rb
+++ lib/breweries/name.rb
@@ -1,20 +1,20 @@
-class Names::Cli
-     attr_accessor :name, :location
-   @@all = []
-   def initialize(name)
-     @name = name
-     @location = location
-     save
-    end 
+# class Names::Cli
+#     attr_accessor :name, :location
+#   @@all = []
+#   def initialize(name)
+#     @name = name
+#     @location = location
+#     save
+#     end 
      
-    def self.all
-       @@all
-   end
+#     def self.all
+#       @@all
+#   end
    
-   def save
-     @@all << self
-   end
+#   def save
+#     @@all << self
+#   end
   
   
-end
-end
+# end
+# end
